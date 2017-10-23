d10758bb5b0530f0d5ffcccfb7ef9161feeebcf9
Luis Fernando <31420386+Fall1393@users.noreply.github.com>
2017-10-22 21:23:45 -0500

    examen2
    este es el examen para la unidad 2

---
+++ Examen/src/Examenunidad2.java
@@ -0,0 +1,59 @@
+    import java.util.Scanner;
+    import java.util.Stack;
+        class Convertir {
+            private Convertir conv;
+            public  static  void main(String[]args){
+                int a,n = 2355;
+                String x = "";
+                Stack pila = new Stack();
+                do {pila.add(a = n % 2);
+                    x = a + x;
+                    n = n / 2;} while (n > 0);
+                    System.out.println("la conversion de decimal a binario es: [ " + pila.push(x) + " ]");}
+                    private void convertir(int decimal) {
+                    Scanner entrada = new Scanner( System.in );
+                }
+        }
+
+
+        public class Examenunidad2 {
+        public static void main(String[] args) {
+            Scanner lee = new Scanner(System.in);
+            int mayor, menor, medio;
+            int a=10;
+            int b=9;
+            int c=8;
+        /*System.out.println("1. Numero Ingresado:  ");
+        a= lee.nextInt();
+        System.out.println("2. Numero Ingresado:  ");
+        b= lee.nextInt();
+        System.out.println("3. Numero Ingresado:  ");
+        c= lee.nextInt();*/
+            if (a>b && a>c){mayor=a;}
+
+            else if(b>a && b>c){mayor=b;}
+
+            else {mayor=c;}
+
+            if (a<b && a<c){menor=a;}
+
+            else if (b<a && b<c){menor=b;}
+
+            else{menor=c;}
+
+            medio=(a+b+c) - (mayor + menor);
+            System.out.println("el numero decendente es: ");
+            System.out.println(menor + " "+ medio + " "+ mayor);
+
+            medio=(a+b+c) - (menor + mayor);
+            System.out.println("el numero acendente es: ");
+            System.out.println(mayor + " "+ medio + " "+ menor);
+        }
+    }
+
+
+
+
+
+
+
